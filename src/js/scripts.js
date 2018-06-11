069ea104e9c7c3fef81f772f1ab0ec3a75837b91
pixelaria <info@pixelaria.ru>
2018-06-11 16:27:48 +0300

    mobile without menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,39 @@
 $(function (){
   console.log('init');
   
+  $('.select__placeholder').click(function(e){
+    $(this).parent().toggleClass('select--active');
+  });
+
+  $('.select__item').click(function(e){
+    console.log('select__item');
+    
+    var val = $(this).data('value');
+    var text = $(this).html();
+    
+    var $select = $(this).closest('.select');
+    var $input = $select.find('.select__input');
+    var $placehoder = $select.find('.select__placeholder');
+
+    $input.val(val);
+    $placehoder.html(text);
+    $select.removeClass('select--active');
+  });
+
+  $('.radio__item').click(function(e){
+    console.log('select__item');
+    
+    var val = $(this).data('value');
+    var text = $(this).html();
+    
+    var $select = $(this).closest('.select');
+    var $input = $select.find('.select__input');
+    var $placehoder = $select.find('.select__placeholder');
+
+    $input.val(val);
+    $placehoder.html(text);
+    $select.removeClass('select--active');
+  });
 });
 
 
